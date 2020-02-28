ffaecfe9dae334273ea86856c74a82912975d014
David A. Dahlbom <david.dahlbom@gmail.com>
2020-02-28 17:58:54 -0500

    trying to track down spasm problem

--- DelNetSTDP.jl
+++ DelNetSTDP.jl
@@ -25,49 +25,53 @@ a_inh = 0.1
 d_exc = 8.0
 d_inh = 2.0
 
-w_exc = 6.0 * 0.5
+w_exc = 5.5
 w_inh = -5.0
 
-τ_pre = 0.034
-A_pre = 51.0
-τ_post = 0.014
-A_post = 103.0
+# τ_pre = 0.034
+# A_pre = 51.0
+# τ_post = 0.014
+# A_post = 103.0
+τ_pre = 0.020
+A_pre = 0.12
+τ_post = 0.020
+A_post = 0.1
 
-syn_max = 20.0
+syn_max = 10.0
 
 # -------------------- Parameters --------------------
 fs = 1000.0
 dt = 1/fs
-dur = 1.0 
+dur = 0.5
 ts = collect(0:dt:dur)
 n_exc = 800
 n_inh = 200
 n = n_exc + n_inh 		# number of elements
-p = 0.1 		
+p = 0.100
 d_max = 20
 verbose = false
-noise = 0.0 	# probability of random firing
+noise = 0.0 			# probability of random firing
 num_steps = 1000
 
 # ---------- Generate Network, Nodes and Delay Lines --------------------
 
 # Generate graph
 g = blobgraph(n, p, collect(1:d_max))
-g[n_exc+1:end,:] = map(x -> x != 0 ? 1 : 0, g[n_exc+1:end,:])
+g[n_exc+1:end,:] = map(x -> x != 0 ? 1 : 0, g[n_exc+1:end,:]) # inh delays = 1
 dn = delnetfromgraph(g)
 
 # Initialize neurons
-neurons = [Neuron() for _ ∈ 1:n]
-synapses = [Float64[] for _ ∈ 1:n]
-trace_pre = [Float64[] for _ ∈ 1:n_exc]
-spike_pre = [Float64[] for _ ∈ 1:n_exc]
+neurons    = [Neuron() for _ ∈ 1:n]
+synapses   = [Float64[] for _ ∈ 1:n]
+trace_pre  = [Float64[] for _ ∈ 1:n_exc]
+spike_pre  = [Float64[] for _ ∈ 1:n_exc]
 trace_post = zeros(n_exc)
 spike_post = zeros(n_exc)
 
 for i ∈ 1:n_exc
 	neurons[i].a = a_exc
 	neurons[i].d = d_exc 
-	synapses[i] = 6.0 .* ones(dn.nodes[i].num_in)
+	synapses[i]  =  w_exc .* ones(dn.nodes[i].num_in)
 	trace_pre[i] = zeros(dn.nodes[i].num_in)
 	spike_pre[i] = zeros(dn.nodes[i].num_in)
 end
@@ -75,7 +79,7 @@ end
 for i ∈ n_exc+1:n
 	neurons[i].a = a_inh
 	neurons[i].d = d_inh
-	synapses[i] = -5.0 .* ones(dn.nodes[i].num_in)
+	synapses[i]  = w_inh .* ones(dn.nodes[i].num_in)
 end
 
 
@@ -87,6 +91,7 @@ rand_count = 0
 synapse_w = zeros(length(ts))
 syn_trace = zeros(length(ts))
 neu_trace = zeros(length(ts))
+potential = zeros(length(ts))
 
 for (k, t) ∈ enumerate(ts)
 	global dn, spikes, rand_count
@@ -104,7 +109,8 @@ for (k, t) ∈ enumerate(ts)
 		if k <= n_exc
 			for p ∈ 1:length(inputs)
 				spike_pre[k][p] = inputs[p]
-				trace_pre[k][p] = min(1.0, trace_pre[k][p]*(1.0-(dt/τ_pre))+spike_pre[k][p])
+				#trace_pre[k][p] = min(1.0, trace_pre[k][p]*(1.0-(dt/τ_pre))+spike_pre[k][p])
+				trace_pre[k][p] = trace_pre[k][p]*(1.0-(dt/τ_pre))+spike_pre[k][p]
 			end
 		end
 
@@ -134,15 +140,16 @@ for (k, t) ∈ enumerate(ts)
 		# Update neuron trace
 		if k <= n_exc
 			spike_post[k] = outval 
-			trace_post[k] = min(1.0, trace_post[k]*(1.0 - (dt/τ_post))+spike_post[k])
+			# trace_post[k] = min(1.0, trace_post[k]*(1.0 - (dt/τ_post))+spike_post[k])
+			trace_post[k] = trace_post[k]*(1.0 - (dt/τ_post))+spike_post[k]
 		end
 
 		# Update synapses
 		if k <= n_exc
 			for p ∈ 1:length(inputs)
 				synapses[k][p] = synapses[k][p] +
-								dt * (A_pre * trace_pre[k][p] * spike_post[k] -
-									  A_post * trace_post[k] * spike_pre[k][p])
+								dt * (A_post * trace_pre[k][p] * spike_post[k] -
+									  A_pre * trace_post[k] * spike_pre[k][p])
 			end
 			synapses[k] = clamp.(synapses[k], 0, syn_max)
 		end
@@ -157,6 +164,7 @@ for (k, t) ∈ enumerate(ts)
 	synapse_w[k] = synapses[1][1]
 	syn_trace[k] = trace_pre[1][1]
 	neu_trace[k] = trace_post[1]
+	potential[k] = neurons[1].v
 end
 
 spikes = spikes[2:end,:]
