c3ea5e5e3299e6c6c8051e3a99567871e5b7f124
analogouscircuit <david.dahlbom@gmail.com>
2020-11-26 12:58:57 -0500

    more retabbing

--- src/pgsearch.h
+++ src/pgsearch.h
@@ -16,18 +16,18 @@
  *  Function Declarations
  *************************************************************/
 idx_t pg_getcontributors(su_mpi_model_l *m,
-	    			     idx_t **sourcenodes,
-	    			     idx_t **numbufferspernode,
-	    			     int commrank, int commsize);
+                         idx_t **sourcenodes,
+                         idx_t **numbufferspernode,
+                         int commrank, int commsize);
 
 idx_t pg_getcontributorweights(su_mpi_model_l *m,  data_t **weights, int commrank, int commsize);
 
 idx_t pg_getcontributordelays(su_mpi_model_l *m,  delay_t **delays, int commrank, int commsize);
 
 idx_t *pg_updateposition(idx_t *positions, idx_t numpositions,
-				    idx_t which, idx_t maxidx);
+                    idx_t which, idx_t maxidx);
 
 int pg_findpgs(su_mpi_model_l *m, su_mpi_trialparams tp, idx_t groupsize, data_t threshold, idx_t maxgroups,
-				spikerecord *sr, MPI_Datatype mpi_spike_type, int commrank, int commsize);
+                spikerecord *sr, MPI_Datatype mpi_spike_type, int commrank, int commsize);
 
 #endif
--- src/pgsearchmain.c
+++ src/pgsearchmain.c
@@ -17,64 +17,64 @@
  *************************************************************/
 int main(int argc, char *argv[])
 {
-	su_mpi_model_l *m;
-	int commsize, commrank;
+    su_mpi_model_l *m;
+    int commsize, commrank;
 
-	/* Init MPI */
-	MPI_Init(&argc, &argv);	
-	MPI_Comm_size(MPI_COMM_WORLD, &commsize);
-	MPI_Comm_rank(MPI_COMM_WORLD, &commrank);
+    /* Init MPI */
+    MPI_Init(&argc, &argv); 
+    MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &commrank);
 
-	/* Set up MPI Datatype for spikes */
-	MPI_Datatype mpi_spike_type = sr_commitmpispiketype();
+    /* Set up MPI Datatype for spikes */
+    MPI_Datatype mpi_spike_type = sr_commitmpispiketype();
 
-	/* Check number of inputs (needs one) */
-	if (argc != 6) {
-		printf("Need five arguments: (1) model name; (2) base size; (3) threshold; (4) duration; (5) max number of groups.\n");
-		exit(-1);
-	}
+    /* Check number of inputs (needs one) */
+    if (argc != 6) {
+        printf("Need five arguments: (1) model name; (2) base size; (3) threshold; (4) duration; (5) max number of groups.\n");
+        exit(-1);
+    }
 
-	char *in_name = argv[1];
+    char *in_name = argv[1];
 
-	/* Load model */
-	m = su_mpi_globalload(in_name, commrank, commsize);
+    /* Load model */
+    m = su_mpi_globalload(in_name, commrank, commsize);
 
-	/* Set up trial parameters */
-	su_mpi_trialparams tp;
-	tp.dur = atof(argv[4]); // make parameter of executable
-	tp.lambda = 0.01; 
-	tp.randspikesize = 0.0;
-	tp.randinput = 1.0;
-	tp.inhibition = 1.0;
-	tp.inputmode = 1.0;
-	tp.multiinputmode = 4.0; // one-shot input at start
-	tp.inputweight = 20.0;
-	tp.recordstart = 0.0;
-	tp.recordstop = 1000.0;
-	tp.lambdainput = 1.0;
-	tp.inputrefractorytime = 1.0;
+    /* Set up trial parameters */
+    su_mpi_trialparams tp;
+    tp.dur = atof(argv[4]); // make parameter of executable
+    tp.lambda = 0.01; 
+    tp.randspikesize = 0.0;
+    tp.randinput = 1.0;
+    tp.inhibition = 1.0;
+    tp.inputmode = 1.0;
+    tp.multiinputmode = 4.0; // one-shot input at start
+    tp.inputweight = 20.0;
+    tp.recordstart = 0.0;
+    tp.recordstop = 1000.0;
+    tp.lambdainput = 1.0;
+    tp.inputrefractorytime = 1.0;
 
-	/* set up spike recorder on each rank */
-	char srname[MAX_NAME_LEN];
-	char rankstr[MAX_NAME_LEN];
-	sprintf(rankstr, "_%d_%d", commsize, commrank);
-	strcpy(srname, in_name);
-	strcat(srname, rankstr);
-	strcat(srname, "_spikes.txt");
-	spikerecord *sr = sr_init(srname, SPIKE_BLOCK_SIZE);
+    /* set up spike recorder on each rank */
+    char srname[MAX_NAME_LEN];
+    char rankstr[MAX_NAME_LEN];
+    sprintf(rankstr, "_%d_%d", commsize, commrank);
+    strcpy(srname, in_name);
+    strcat(srname, rankstr);
+    strcat(srname, "_spikes.txt");
+    spikerecord *sr = sr_init(srname, SPIKE_BLOCK_SIZE);
 
-	/* perform the search */
-	pg_findpgs(m, tp, atoi(argv[2]), atof(argv[3]), atoi(argv[5]), sr, mpi_spike_type, commrank, commsize);
+    /* perform the search */
+    pg_findpgs(m, tp, atoi(argv[2]), atof(argv[3]), atoi(argv[5]), sr, mpi_spike_type, commrank, commsize);
 
-	/* Clean up */
-	char srfinalname[256];
-	strcpy(srfinalname, in_name);
-	strcat(srfinalname, "pg_spikes.txt");
-	sr_collateandclose(sr, srfinalname, commrank, commsize, mpi_spike_type);
+    /* Clean up */
+    char srfinalname[256];
+    strcpy(srfinalname, in_name);
+    strcat(srfinalname, "pg_spikes.txt");
+    sr_collateandclose(sr, srfinalname, commrank, commsize, mpi_spike_type);
 
-	su_mpi_freemodel_l(m);
+    su_mpi_freemodel_l(m);
 
-	MPI_Finalize();
+    MPI_Finalize();
 
-	return 0;
+    return 0;
 }
